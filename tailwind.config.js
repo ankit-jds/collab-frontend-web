e63106605f74cf28fd082d5153b78120505f12ef
ankit-jds <84713907+ankit-jds@users.noreply.github.com>
2025-01-04 20:41:47 +0530

    ADD: tailwind css, react query, lucide react packages

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html", // Include the HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Include your JS/JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
