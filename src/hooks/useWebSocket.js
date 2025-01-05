afce0eccc614e1b5e77832793a1b16cfc820a1e7
ankit-jds <84713907+ankit-jds@users.noreply.github.com>
2025-01-06 03:06:03 +0530

    ADD: useWebSocket hook, websocket connection to document content

---
+++ src/hooks/useWebSocket.js
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from "react";
+
+const useWebSocket = (url, options = {}) => {
+  const {
+    onOpen,
+    onClose,
+    onMessage,
+    onError,
+    reconnect = true,
+    reconnectInterval = 3000,
+  } = options;
+  const socketRef = useRef(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const reconnectTimeoutRef = useRef(null);
+
+  useEffect(() => {
+    if (!url) return;
+    const connectWebSocket = () => {
+      const socket = new WebSocket(url);
+      socketRef.current = socket;
+
+      socket.onopen = () => {
+        setIsConnected(true);
+        if (onOpen) onOpen(socket);
+      };
+
+      socket.onclose = () => {
+        setIsConnected(false);
+        if (onClose) onClose(socket);
+
+        // if (reconnect) {
+        //   reconnectTimeoutRef.current = setTimeout(() => {
+        //     connectWebSocket();
+        //   }, reconnectInterval);
+        // }
+      };
+
+      socket.onmessage = (event) => {
+        if (onMessage) onMessage(JSON.parse(event.data));
+      };
+
+      socket.onerror = (error) => {
+        if (onError) onError(error);
+      };
+    };
+
+    connectWebSocket();
+
+    return () => {
+      console.log("WebSocket readyState:", socketRef.current.readyState);
+      if (socketRef.current) {
+        socketRef.current.close();
+        socketRef.current = null;
+      }
+      //   clearTimeout(reconnectTimeoutRef.current);
+    };
+    //   }, [url, onOpen, onClose, onMessage, onError, reconnect, reconnectInterval]);
+  }, [url]);
+
+  const sendMessage = (message) => {
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      socketRef.current.send(JSON.stringify(message));
+    } else {
+      console.warn("WebSocket is not open. Cannot send message.");
+    }
+  };
+  return { isConnected, sendMessage };
+};
+
+export default useWebSocket;
--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import { useDocumentAPI } from "../hooks/useDocumentAPI";
+import useWebSocket from "../hooks/useWebsocket";
 
 const Editor = () => {
   const {
@@ -7,14 +9,36 @@ const Editor = () => {
     isLoading: isLoading1,
     error: error1,
   } = useDocumentAPI({ documentId: 5 });
+  console.log("hahaha......ahahahahah..... infinite reloading hori hai");
+  const [content, setContent] = useState("");
+  const wsurl = documentData ? `ws://127.0.0.1:8420/ws/document/5/` : null;
+  const { isConnected, sendMessage } = useWebSocket(wsurl, {
+    onOpen: () => console.log("websocket connected"),
+    onClose: () => console.log("websocket disconnected"),
+    onMessage: (message) => {
+      console.log(message, typeof message, "websocket message aaya hai");
+      if (typeof message === "object") {
+        setContent(message?.content);
+      } else {
+        // apply operations code here...
+        console.log(message, "operations has arrived");
+      }
+    },
+    onError: (error) => console.log(error, "websocket error"),
+  });
   if (isLoading1) return <div>Loading...</div>;
   if (error1) return <div>Error in API 1: {error1.message}</div>;
+  console.log(isConnected, "mlml");
 
   return (
     <div className="flex flex-col h-lvh">
       <Header documentNameProp={documentData?.data?.name} />
+      <div>websocket : {isConnected ? "CONNECTED✅" : "DISCONNECTED❌"}</div>
       <div className="flex-grow p-10">
-        <textarea className="h-full w-full border p-2 resize-none"></textarea>
+        <textarea
+          className="h-full w-full border p-2 resize-none"
+          value={content}
+        ></textarea>
       </div>
     </div>
   );
